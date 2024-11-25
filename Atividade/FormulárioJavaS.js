f1e08da39c38884a13f01886c4de5a322ffb4a2f
Emanuel Vitor Felix de Oliveira <81042722+N3M3S8S@users.noreply.github.com>
2024-11-25 13:24:42 -0300

    Correção/Pattern

--- Atividade/FormulárioJavaS.js
+++ Atividade/FormulárioJavaS.js
@@ -5,3 +5,11 @@ function showDiv() {
 function hideDiv() {
     document.getElementById('ex').style.display = "none";
 }
+
+function addPoint(){
+    var inputValue = document.getElementById("CEP").value;
+    var inputValueLength = inputValue.length;
+    if(inputValueLength == 5){
+        document.getElementById("CEP").value = inputValue+"-"; 
+    }
+   }
