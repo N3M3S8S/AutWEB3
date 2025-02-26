53247c7686aeaacc7beb153e2efe18306f0624d5
Emanuel Vitor - NITROV15 <emanuellvitor500@gmail.com>
2025-02-25 23:14:20 -0300

    Bruh

--- Projeto-AUTWEB - Atualizado - 26_01 - Noite/JavaScript/Create.js
+++ Projeto-AUTWEB - Atualizado - 26_01 - Noite/JavaScript/Create.js
@@ -51,6 +51,8 @@ function verificaCamposCriar() {
     }
 }
 
+
+
 function verCamposOpt () {
     changeDisplay('opt', 'final')
 }
--- Projeto-AUTWEB - Atualizado - 26_01 - Noite/JavaScript/Forum.js
+++ Projeto-AUTWEB - Atualizado - 26_01 - Noite/JavaScript/Forum.js
@@ -8,6 +8,24 @@ function changePostsOfc() {
     document.getElementById('cen').classList.remove('public')
 }
 
+function busca() {
+    let se = document.getElementById('sear').value;
+    let j = 1
+    document.getElementById('resu').innerHTML = ""
+
+    while (j <= QntContas) {
+        let conta = JSON.parse(localStorage.getItem('conta' + j))
+        let ap = conta.apelido.toString()
+        if (ap.includes(se) == true && se != "") {
+            let nUs = document.createElement('div')
+            nUs.classList.add('user')
+            nUs.innerHTML = '<img src="Imagens/PFP_Holder.jpg" alt=""><span>' + ap +'</span>'
+            document.getElementById('resu').appendChild(nUs)
+        }
+        j++
+    }
+}
+
 window.onload = function( ) {
     changeTheme()
 }
